851e544afd2c79dd4ff5a97b8294d53ff2edb2d1
seanmcquaid <sean.mcquaid2@verizon.net>
2021-01-13 11:45:00 -0500

    learned about props in svelte

---
+++ props/Nested.svelte
@@ -0,0 +1,6 @@
+<script>
+  // default value can be overriden by passed in prop
+  export let answer = "hello";
+</script>
+
+<p>The answer is {answer}</p>
---
+++ props/SpreadProps.svelte
@@ -0,0 +1,17 @@
+<script>
+  import ArraysObjects from "../reactivity/ArraysObjects.svelte";
+  import Statements from "../reactivity/Statements.svelte";
+  import DeclaringProps from "./DeclaringProps.svelte";
+  import DefaultValues from "./DefaultValues.svelte";
+  import Info from "./Info.svelte";
+  import Nested from "./Nested.svelte";
+
+  const pkg = {
+    name: "svelte",
+    version: 3,
+    speed: "blazing",
+    website: "https://svelte.dev",
+  };
+</script>
+
+<Info {...pkg} />
