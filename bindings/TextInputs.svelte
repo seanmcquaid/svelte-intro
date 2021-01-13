fa59c46ecf301e1f1f1c31a5e480212e70eab584
seanmcquaid <sean.mcquaid2@verizon.net>
2021-01-13 16:24:21 -0500

    got most of the way through bindings

---
+++ bindings/TextInputs.svelte
@@ -0,0 +1,8 @@
+<script>
+  let name = 'world';
+</script>
+
+<input value={name} />
+
+<h1>Hello {name}!</h1>
+<input bind:value={name} />
---
+++ bindings/TextareaInputs.svelte
@@ -0,0 +1,15 @@
+<script>
+  import marked from 'marked';
+  let value = `Some words are *italic*, some are **bold**`;
+</script>
+
+<textarea bind:value />
+
+{@html marked(value)}
+
+<style>
+  textarea {
+    width: 100%;
+    height: 200px;
+  }
+</style>
