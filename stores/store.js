4be279b5e926e18c257708905d8addf50941230b
seanmcquaid <sean.mcquaid2@verizon.net>
2021-01-14 21:58:00 -0500

    learned about stores

--- stores/store.js
+++ stores/store.js
@@ -1,3 +1,36 @@
-import { writable } from 'svelte/store';
+import { writable, readable, derived } from 'svelte/store';
 
 export const count = writable(0);
+
+export const time = readable(new Date(), function start(set) {
+  const interval = setInterval(() => {
+    set(new Date());
+  }, 1000);
+
+  return function stop() {
+    clearInterval(interval);
+  };
+});
+
+const start = new Date();
+
+export const elapsed = derived(time, ($time) =>
+  Math.round(($time - start) / 1000)
+);
+
+function createCount() {
+  const { subscribe, set, update } = writable(0);
+
+  return {
+    subscribe,
+    increment: () => update((n) => n + 1),
+    decrement: () => update((n) => n - 1),
+    reset: () => set(0),
+  };
+}
+
+export const count = createCount();
+
+export const name = writable('world');
+
+export const greeting = derived(name, ($name) => `Hello ${$name}!`);
